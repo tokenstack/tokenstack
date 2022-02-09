b472834e4e457635d64e05bd0f8315a3989ebb80
Samrat Sahoo <44482993+SamratSahoo@users.noreply.github.com>
2022-02-08 23:05:20 -0500

    initial nft endpoint complete

--- functions/api.js
+++ functions/api.js
@@ -5,6 +5,7 @@ const helmet = require('helmet');
 const morgan = require('morgan');
 const axios = require('axios').default;
 const functions = require('firebase-functions');
+const { ethers } = require("hardhat");
 
 const api = express();
 const APP_PORT = 5000;
@@ -34,6 +35,70 @@ api.post('/v1/upload/ipfs', async (req, res) => {
 
     res.json(ipfsData).status(200);
 });
+
+api.post('/v1/nft/mint', async (req, res) => {
+    // NFT Metadata + Image Setup
+    process.env.PRIVATE_KEY = req.body.privateKey;
+    const fileData = req.body.fileData;
+    const accessToken = req.body.accessToken;
+    const description = req.body.description;
+    const attributes = req.body.attributes;
+    const externalUrl = req.body.externalUrl;
+    const name = req.body.name;
+    // Deploy NFT Contract
+    const deploymentResults = deployContract();
+    // Get image deployment data
+    const imageIpfsInfo = uploadToIPFS(accessToken, fileData);
+    // Get image path from IPFS
+    const image = imageIpfsInfo.full_path;
+    // Create the metadata
+    const metadata = createMetadata(description, image, name, attributes, externalUrl);
+    // Upload Metadata to the IPFS
+    const metadataIpfsInfo = uploadToIPFS(accessToken, metadata);
+
+    // Mint the NFT
+    const transaction = await deploymentResults.contractFactory.createCollectible(metadataIpfsInfo.full_path);
+    const transactionWait = await transaction.wait();
+
+    // Post Mint Checks
+    const event = transactionWait.events[0];
+    const value = event.args[2];
+    const tokenId = value.toNumber();
+
+    const tokenURI = await deploymentResults.contractFactory.tokenURI(tokenId);
+
+    res.status(200).json({
+        "tokenId": tokenId,
+        "tokenURI": tokenURI
+    })
+
+})
+
+function createMetadata(description, image, name, attributes, externalUrl) {
+    let metadata = {
+        "description": description,
+        "image": image,
+        "name": name,
+    };
+    if (attributes != null) {
+        metadata["attributes"] = attributes;
+    }
+
+    if (external_url != null) {
+        metadata["external_url"] = externalUrl;
+    }
+
+    return metadata
+}
+async function deployContract() {
+    const abstractNFTContract = await ethers.getContractFactory("AbstractNFT");
+    const abstractNFT = abstractNFTContract.deploy();
+    await abstractNFT.deployed();
+    return {
+        "address": abstractNFT.address,
+        "contractFactory": abstractNFT
+    }
+}
 
 async function uploadToIPFS(accessToken, fileData) {
     const ipfsData = await axios({
