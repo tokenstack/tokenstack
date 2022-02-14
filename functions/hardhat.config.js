069a84661c99f5b4680382053c29487a719465de
Samrat Sahoo <44482993+SamratSahoo@users.noreply.github.com>
2022-02-13 21:40:21 -0500

    string error with node url

--- functions/hardhat.config.js
+++ functions/hardhat.config.js
@@ -8,7 +8,7 @@ require('@openzeppelin/hardhat-upgrades');
 const crypto = require("crypto");
 
 const PRIVATE_KEY = process.env.PRIVATE_KEY ? process.env.PRIVATE_KEY : crypto.randomBytes(32).toString('hex');
-const NODE_URL = process.env.NODE_URL;
+const NODE_URL = process.env.NODE_URL ? process.env.NODE_URL : crypto.randomBytes(32).toString('hex');
 
 module.exports = {
   solidity: "0.8.0",
